61ba4f92ff0879aa926cf7314ecb5aa2b6aa775a
= <nncl@live.com>
2017-09-10 17:02:33 -0300

    Movendo o fundo

---
+++ Assets/Scripts/MoverFundo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverFundo : MonoBehaviour {
+
+	public float limiteY, velocidade;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector2.down * velocidade * Time.deltaTime);
+
+		if (transform.position.y <= limiteY) {
+			transform.position = new Vector2 (0.0f, limiteY * -1);
+		}
+	}
+}
