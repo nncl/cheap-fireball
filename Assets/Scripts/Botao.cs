5e115657a1fcebf4319433483817637c05c379d1
= <nncl@live.com>
2017-09-10 17:38:36 -0300

    Add tela de início

---
+++ Assets/Scripts/Botao.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Botao : MonoBehaviour {
+
+	public Button botaoInicio;
+	Button btn;
+
+	// Use this for initialization
+	void Start () {
+		btn = botaoInicio.GetComponent<Button> ();
+		btn.onClick.AddListener (EntrarNoJogo);
+	}
+	
+	void EntrarNoJogo () {
+		SceneManager.LoadScene ("Game");
+	}
+}
