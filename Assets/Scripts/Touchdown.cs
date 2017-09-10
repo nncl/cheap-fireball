5dc3e4d87a0f369cba18e4ef8acb0d48a6eff6f9
= <nncl@live.com>
2017-09-10 20:45:46 -0300

    Tela final, após touhdown

--- Assets/Scripts/Touchdown.cs
+++ Assets/Scripts/Touchdown.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Touchdown : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D c) {
 		if (c.gameObject.tag == "Jogador") {
 			print ("TOUCH DOWWWWWN");
+			SceneManager.LoadScene ("Final");
 		}
 	}
 }
